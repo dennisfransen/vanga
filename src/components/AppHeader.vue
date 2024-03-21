157b0fc090bc39361fbe601d7c289764537d7a52
Dennis Fransen <dennis.fransen@trinax.se>
2024-03-21 22:32:13 +0100

    before animation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,27 +1,17 @@
 <script setup lang="ts">
-import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
+// import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
 	<header class="border-b bg-white">
-		<div class="flex h-20 items-center justify-between pl-4 pr-8">
-			<div class="relative">
-				<label for="search" class="hidden">Sök</label>
-				<input type="search" placeholder="Sök..." class="w-full border-none pl-10" />
-				<magnifying-glass-icon
-					class="container absolute left-0 top-1/2 h-6 w-6 -translate-y-1/2 bg-red-500 text-gray-400"
-				/>
-			</div>
-
-			<nav class="flex items-center gap-6">
-				<router-link
-					to="/register"
-					class="flex items-center gap-1 rounded-md bg-blue-100 py-2 pl-5 pr-3 font-semibold text-blue-500"
-				>
-					<span>Logga in</span>
-					<span class="material-symbols-rounded">chevron_right</span>
-				</router-link>
-			</nav>
+		<div class="flex h-[4.5rem] items-center justify-between pl-4 pr-8">
+			<!--			<div class="relative">-->
+			<!--				<label for="search" class="hidden">Sök</label>-->
+			<!--				<input type="search" placeholder="Sök..." class="w-full border-none pl-10" />-->
+			<!--				<magnifying-glass-icon-->
+			<!--					class="absolute left-0 top-1/2 h-6 w-6 -translate-y-1/2 text-gray-400"-->
+			<!--				/>-->
+			<!--			</div>-->
 		</div>
 	</header>
 </template>
--- src/components/AppSideMenu.vue
+++ src/components/AppSideMenu.vue
@@ -10,105 +10,63 @@ import {
 </script>
 
 <template>
-	<aside class="flex min-h-screen min-w-72 flex-col bg-white">
-		<div class="flex h-20 items-center py-4 pl-8 pr-4 text-lg font-bold tracking-wider">
-			<router-link to="/">
+	<aside class="flex min-h-screen min-w-[4.5rem] flex-col space-y-6 bg-white">
+		<section class="grid h-[4.5rem] place-items-center">
+			<router-link to="/" class="inline-block min-w-10 text-center font-black">
 				<span class="text-blue-500">M</span>
 				<span>M</span>
 			</router-link>
-		</div>
+		</section>
 
-		<div class="space flex flex-grow flex-col space-y-6 p-4">
-			<nav>
-				<ul>
-					<li>
-						<router-link to="/dashboard" class="link" active-class="active">
-							<home-icon class="h-6 w-6" />
-							<span>Översikt</span>
-						</router-link>
-					</li>
-					<li>
-						<router-link to="/member" class="link" active-class="active">
-							<heart-icon class="h-6 w-6" />
-							<span>Medlemmar</span>
-						</router-link>
-					</li>
-					<li>
-						<router-link to="/invoice" class="link" active-class="active">
-							<currency-dollar-icon class="h-6 w-6" />
-							<span>Fakturering</span>
-						</router-link>
-					</li>
-					<li>
-						<router-link to="/membership" class="link" active-class="active">
-							<bolt-icon class="h-6 w-6" />
-							<span>Medlemskap</span>
-						</router-link>
-					</li>
-					<li>
-						<router-link to="/report" class="link" active-class="active">
-							<arrow-trending-up-icon class="h-6 w-6" />
-							<span>Rapporter</span>
-						</router-link>
-					</li>
-				</ul>
-			</nav>
+		<section class="flex flex-grow flex-col items-center gap-y-6">
+			<router-link to="/dashboard" class="link" active-class="active">
+				<home-icon class="h-6 w-6" />
+				<span class="link-title">Dashboard</span>
+			</router-link>
+			<router-link to="/member" class="link" active-class="active">
+				<heart-icon class="h-6 w-6" />
+				<span class="link-title">Medlemmar</span>
+			</router-link>
+			<router-link to="/invoice" class="link" active-class="active">
+				<currency-dollar-icon class="h-6 w-6" />
+				<span class="link-title">Fakturering</span>
+			</router-link>
+			<router-link to="/membership" class="link" active-class="active">
+				<bolt-icon class="h-6 w-6" />
+				<span class="link-title">Medlemskap & Tillval</span>
+			</router-link>
+			<router-link to="/report" class="link" active-class="active">
+				<arrow-trending-up-icon class="h-6 w-6" />
+				<span class="link-title">Rapporter</span>
+			</router-link>
+			<hr class="w-4" />
+		</section>
 
-			<nav>
-				<span class="inline-block pb-2 text-sm font-bold text-gray-600">Klubbar</span>
-				<ul>
-					<li>
-						<router-link to="/1" class="link" active-class="active">
-							<span
-								class="grid aspect-square w-8 place-items-center rounded-md border border-gray-300 text-sm"
-							>
-								NÄ
-							</span>
-							<span>Norra Älvsborgs Ryttarklubb</span>
-						</router-link>
-					</li>
-					<li>
-						<router-link to="/2" class="link" active-class="active">
-							<span
-								class="grid aspect-square w-8 place-items-center rounded-md border border-gray-300 text-sm"
-							>
-								LE
-							</span>
-							<span>Lilla Edets Ridklubb</span>
-						</router-link>
-					</li>
-				</ul>
-			</nav>
-		</div>
-
-		<nav class="flex h-16 items-center px-4 py-10">
-			<ul class="w-full">
-				<li>
-					<router-link to="/settings" class="link" active-class="active">
-						<cog-icon class="h-6 w-6" />
-						<span>Inställningar</span>
-					</router-link>
-				</li>
-			</ul>
-		</nav>
+		<section class="grid h-[4.5rem] place-items-center">
+			<router-link to="/setting" class="link" active-class="active">
+				<cog-icon class="h-6 w-6" />
+				<span class="link-title">Inställningar</span>
+			</router-link>
+		</section>
 	</aside>
 </template>
 
 <style scoped lang="postcss">
+aside {
+	@apply hover:min-w-72 hover:px-4;
+}
+
 .link {
-	@apply flex items-center gap-3;
-	@apply rounded-md p-2 text-gray-500;
+	@apply inline-block h-10 w-10;
+	@apply grid items-center justify-center rounded-md;
 }
 
 .active {
-	@apply bg-blue-50 font-semibold text-blue-500;
+	@apply bg-blue-200 text-blue-500;
 }
 
-.material-symbols-rounded {
-	@apply rounded-md p-1;
-}
-
-.active .material-symbols-rounded {
-	/*@apply bg-blue-100;*/
+.link-title {
+	@apply hidden;
+	@apply group-hover:inline;
 }
 </style>
