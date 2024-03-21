b70cf7ed778fd693492ce5932c02312aab260200
Dennis Fransen <dennis.fransen@trinax.se>
2024-03-22 00:18:49 +0100

    simplified css

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
-// import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
-	<header class="border-b bg-white">
-		<div class="flex h-[4.5rem] items-center justify-between pl-4 pr-8">
-			<!--			<div class="relative">-->
-			<!--				<label for="search" class="hidden">Sök</label>-->
-			<!--				<input type="search" placeholder="Sök..." class="w-full border-none pl-10" />-->
-			<!--				<magnifying-glass-icon-->
-			<!--					class="absolute left-0 top-1/2 h-6 w-6 -translate-y-1/2 text-gray-400"-->
-			<!--				/>-->
-			<!--			</div>-->
+	<header class="bg-white shadow-sm">
+		<div class="flex h-[4.5rem] items-center justify-between px-4">
+			<div class="relative flex-grow">
+				<label for="search" class="hidden">Sök</label>
+				<input
+					type="search"
+					class="w-full rounded-md border-none pl-10 text-sm placeholder:text-gray-500"
+					placeholder="Sök e-post, förnamn, efternamn, medlemsnummer eller fakturanummer..."
+				/>
+				<magnifying-glass-icon
+					class="absolute left-2 top-1/2 h-6 w-6 -translate-y-1/2 text-gray-500"
+				/>
+			</div>
 		</div>
 	</header>
 </template>
--- src/components/AppSideMenu.vue
+++ src/components/AppSideMenu.vue
@@ -10,34 +10,29 @@ import {
 </script>
 
 <template>
-	<aside class="flex min-h-screen min-w-[4.5rem] flex-col space-y-6 bg-white">
+	<aside class="relative z-10 flex min-h-screen min-w-[4.5rem] flex-col space-y-6 bg-white">
 		<section class="grid h-[4.5rem] place-items-center">
-			<router-link to="/" class="inline-block min-w-10 text-center font-black">
-				<span class="text-blue-500">M</span>
-				<span>M</span>
+			<router-link to="/" class="font-black leading-none">
+				<span class="text-blue-500">Mitt</span> <br />
+				<span class="text-gray-800">Med</span>
 			</router-link>
 		</section>
 
 		<section class="flex flex-grow flex-col items-center gap-y-6">
 			<router-link to="/dashboard" class="link" active-class="active">
 				<home-icon class="h-6 w-6" />
-				<span class="link-title">Dashboard</span>
 			</router-link>
 			<router-link to="/member" class="link" active-class="active">
 				<heart-icon class="h-6 w-6" />
-				<span class="link-title">Medlemmar</span>
 			</router-link>
 			<router-link to="/invoice" class="link" active-class="active">
 				<currency-dollar-icon class="h-6 w-6" />
-				<span class="link-title">Fakturering</span>
 			</router-link>
 			<router-link to="/membership" class="link" active-class="active">
 				<bolt-icon class="h-6 w-6" />
-				<span class="link-title">Medlemskap & Tillval</span>
 			</router-link>
 			<router-link to="/report" class="link" active-class="active">
 				<arrow-trending-up-icon class="h-6 w-6" />
-				<span class="link-title">Rapporter</span>
 			</router-link>
 			<hr class="w-4" />
 		</section>
@@ -45,28 +40,19 @@ import {
 		<section class="grid h-[4.5rem] place-items-center">
 			<router-link to="/setting" class="link" active-class="active">
 				<cog-icon class="h-6 w-6" />
-				<span class="link-title">Inställningar</span>
 			</router-link>
 		</section>
 	</aside>
 </template>
 
 <style scoped lang="postcss">
-aside {
-	@apply hover:min-w-72 hover:px-4;
-}
-
 .link {
-	@apply inline-block h-10 w-10;
-	@apply grid items-center justify-center rounded-md;
+	@apply h-10 w-10 rounded-md;
+	@apply grid place-items-center;
+	@apply transition hover:bg-blue-100;
 }
 
 .active {
-	@apply bg-blue-200 text-blue-500;
-}
-
-.link-title {
-	@apply hidden;
-	@apply group-hover:inline;
+	@apply bg-blue-100 text-blue-500;
 }
 </style>
